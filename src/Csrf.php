39c94ec2bcb6aaf4fa202527d486a3d186f2ed4a
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-05-29 17:10:34 +0200

    Csrf class

---
+++ src/Csrf.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MuhamedDidovic\Csrf;
+
+class Csrf
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Checks if CSRF is valid
+     */
+    public function check()
+    {
+        echo 'Checking Csrf..';
+    }
+}
